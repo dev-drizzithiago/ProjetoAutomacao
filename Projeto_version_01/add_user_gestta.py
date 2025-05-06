cad6bf64aee8929ef6f2357cf29b40925e7ddb4d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-05 21:52:11 -0300

    atualização

--- Projeto_version_01/add_user_gestta.py
+++ Projeto_version_01/add_user_gestta.py
@@ -1,6 +1,10 @@
 """ https://googlechromelabs.github.io/chrome-for-testing/#stable """
-
+from pathlib import Path
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+ROOT_FOLDER = Path(__file__).parent
+PATH_CHROME_DRIVER =
 
 def site_gessta(dados_entrada):
     print('Criando usuário no GESSTA')
