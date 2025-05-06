4636809c256fe62bbc815d4f39a5ede4209cadaf
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-05 21:57:29 -0300

    Update add_user_gestta.py

--- Projeto_version_01/add_user_gestta.py
+++ Projeto_version_01/add_user_gestta.py
@@ -4,7 +4,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 ROOT_FOLDER = Path(__file__).parent
-PATH_CHROME_DRIVER =
+PATH_CHROME_DRIVER = Path(ROOT_FOLDER / 'driver_google' / 'chromedriver.exe')
+
 
 def site_gessta(dados_entrada):
     print('Criando usuário no GESSTA')
