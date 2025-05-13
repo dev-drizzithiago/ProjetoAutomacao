751eeaa6505e7cc26225ded8dd2b46ae3f2f7cb6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-13 16:52:03 -0300

    Update main.py

--- teste_velocidade_v1/main.py
+++ teste_velocidade_v1/main.py
@@ -1,11 +1,20 @@
 import speedtest
 import teste_download
+import teste_upload
+
 
 class TesteVelocidade:
+    def __init__(self):
+        self.modulo_speed = speedtest.Speedtest()
 
-    def testando_conexao(self):
-        return teste_download.TesteDownload(speedtest)
+    def testando_conexao_down(self):
+        return teste_download(self.modulo_speed)
+
+    def testando_conexao_up(self):
+        return teste_upload(self.modulo_speed)
 
 
 iniciando_obj = TesteVelocidade()
-print(iniciando_obj.testando_conexao())
+print(iniciando_obj)
+print(iniciando_obj.testando_conexao_down())
+print(iniciando_obj.testando_conexao_up())
