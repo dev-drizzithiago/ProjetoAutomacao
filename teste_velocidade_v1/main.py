97975a2b15cf12b832c08d10b1554957b3824325
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-13 12:23:41 -0300

    Update main.py

--- teste_velocidade_v1/main.py
+++ teste_velocidade_v1/main.py
@@ -1,13 +1,21 @@
 import speedtest
+import teste_download
 
+class TesteVelocidade:
 
-servidor_teste = st.get_best_server()['name']
+    def testando_conexao(self):
+        return teste_download.TesteDownload(speedtest)
 
-teste_upload = f'{st.upload() / 1_000_000:.2f} mbps'
-teste_ping = f'{st.results.ping} ms'
+# servidor_teste = st.get_best_server()['name']
+#
+# teste_upload = f'{st.upload() / 1_000_000:.2f} mbps'
+# teste_ping = f'{st.results.ping} ms'
+#
+# print(servidor_teste)
+#
+# print(teste_download)
+# print(teste_upload)
+# print(teste_ping)
 
-print(servidor_teste)
-
-print(teste_download)
-print(teste_upload)
-print(teste_ping)
+iniciando_obj = TesteVelocidade()
+print(iniciando_obj.testando_conexao())
