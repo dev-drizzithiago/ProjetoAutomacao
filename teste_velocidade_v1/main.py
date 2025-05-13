34778b3c190991b330f894abe6d694f4197b3bfe
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-13 16:58:17 -0300

    Update main.py

--- teste_velocidade_v1/main.py
+++ teste_velocidade_v1/main.py
@@ -13,6 +13,9 @@ class TesteVelocidade:
         teste_download = f'{self.modulo_speed.upload() / 1_000_000:.2f} mbps'
         return teste_download
 
+    def teste_conexao_tempo_resposta(self):
+        teste_ms = f'{self.modulo_speed.results.ping} ms'
+
 
 iniciando_obj = TesteVelocidade()
 print(iniciando_obj)
