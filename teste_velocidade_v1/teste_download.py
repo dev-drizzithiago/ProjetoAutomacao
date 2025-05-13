9a63ee48bb87e6712e3d4bbd8da58d186159fe6d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-13 12:02:08 -0300

    Update teste_download.py

--- teste_velocidade_v1/teste_download.py
+++ teste_velocidade_v1/teste_download.py
@@ -1,6 +1,6 @@
 class TesteDownload:
     def __init__(self, modulo_speedtest):
-        self.st.modulo_speedtest
+        self.st = modulo_speedtest
 
     def processo_teste(self):
         teste_download = f'{self.st.download() / 1_000_000:.2f} mbps'
