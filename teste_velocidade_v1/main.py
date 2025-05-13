da94f0f969dc574b27d789e45d60fbd7d3c32391
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-13 18:06:26 -0300

    Update main.py

--- teste_velocidade_v1/main.py
+++ teste_velocidade_v1/main.py
@@ -15,9 +15,15 @@ class TesteVelocidade:
 
     def teste_conexao_tempo_resposta(self):
         teste_ms = f'{self.modulo_speed.results.ping} ms'
+        return teste_ms
 
+    def servidor_teste(self):
+        servidor = f'{self.modulo_speed.get_best_server()}'
+        return servidor
 
 iniciando_obj = TesteVelocidade()
-print(iniciando_obj)
+
+print(f'Teste de velocidade para o servidor {iniciando_obj.servidor_teste()}')
 print(iniciando_obj.testando_conexao_down())
 print(iniciando_obj.testando_conexao_up())
+print(iniciando_obj.teste_conexao_tempo_resposta())
