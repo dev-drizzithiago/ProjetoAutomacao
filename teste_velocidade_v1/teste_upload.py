8c77f29af43ce8172f5ef2cbb7c93230e7a0b435
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-13 16:58:20 -0300

    Delete teste_upload.py

--- teste_velocidade_v1/teste_upload.py
+++
@@ -1,9 +0,0 @@
-class TesteUpload:
-    print('Iniciando class TesteUpload')
-
-    def __init__(self, modulo_speedtest):
-        self.st = modulo_speedtest.Speedtest
-
-    def processo_teste(self):
-        teste_download = f'{self.st.download() / 1_000_000:.2f} mbps'
-        return teste_download
