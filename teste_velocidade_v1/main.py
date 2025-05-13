dc833c503637718ff6973eb5b078227a5613f7c4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-13 19:16:51 -0300

    Update main.py

--- teste_velocidade_v1/main.py
+++ teste_velocidade_v1/main.py
@@ -26,7 +26,7 @@ class TesteVelocidade:
     def dados_cliente(self):
         """
         Indica os dados do cliente
-        :return:
+        :return: retorna o endereço de ip e o nome da operadora
         """
         dados_cliente = dict(self.modulo_speed.results.client)
 
@@ -36,17 +36,29 @@ class TesteVelocidade:
         )
         return servidor
 
+linha_formatacao = '- - ' * 20
+contador_teste = 1
 
 while True:
+
+    iniciando_obj = TesteVelocidade()
+    print(f'{contador_teste}° teste de velocidade')
+    print(linha_formatacao)
+
     try:
-        iniciando_obj = TesteVelocidade()
-        print(f'Teste de velocidade para o servidor {iniciando_obj.servidor_teste()}')
-        print(iniciando_obj.testando_conexao_down())
-        print(iniciando_obj.testando_conexao_up())
-        print(iniciando_obj.teste_conexao_tempo_resposta())
-        print(iniciando_obj.dados_cliente()['IP'])
-        print(iniciando_obj.dados_cliente()['Operadora'])
-        sleep(60)
+        print(f'Teste Download: [{iniciando_obj.testando_conexao_down()}]')
+        print(f'Teste Upload: [{iniciando_obj.testando_conexao_up()}]')
+        print(f'Tempo de resposta: [{iniciando_obj.teste_conexao_tempo_resposta()}]')
+
+        print(f'Seu endereço de internet: [{iniciando_obj.dados_cliente()["IP"]}]')
+        print(f'Sua operadora: [{iniciando_obj.dados_cliente()["Operadora"]}]')
+
+        print(f'{contador_teste}° Finalizado!')
+        print(linha_formatacao)
+        sleep(120)
     except speedtest.ConfigRetrievalError:
         print('Você tentou várias em um curto periodo, aguarda alguns minutos')
-        sleep(60)
+        sleep(120)
+
+    print(f'{contador_teste}° Finalizado!')
+    print(linha_formatacao)
