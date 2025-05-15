0afc0c46c1ea97670b0a8d0d2c47789422987216
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-14 21:43:11 -0300

    Update analise_dados.py

--- teste_velocidade_v1/analise_dados.py
+++ teste_velocidade_v1/analise_dados.py
@@ -11,13 +11,14 @@ class AnaliseDados:
 
 if __name__ == '__main__':
     dados_teste_velocidade = {
-        'data_teste': '',
-        'teste_download': '',
-        'teste_upload': '',
-        'tempo_resposta': '',
-        'dados_cliente': ['', ''],
+        'data_teste': '14/05/2025 - 21:39:50',
+        'teste_download': '329.70 mbps',
+        'teste_upload': '351.25 mbps',
+        'tempo_resposta': '6.877 ms',
+        'dados_cliente': ['179.228.37.110', 'Vivo'],
     }
 
     iniciando_obj_analise_dados = AnaliseDados(dados_teste_velocidade)
-    print(iniciando_obj_analise_dados._view_dados)
+
+    print(iniciando_obj_analise_dados._view_dados())
 
