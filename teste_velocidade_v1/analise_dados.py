abcaceab20a17c3e81a9e5ed59d17f56f7c3686a
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-14 21:41:22 -0300

    Update analise_dados.py

--- teste_velocidade_v1/analise_dados.py
+++ teste_velocidade_v1/analise_dados.py
@@ -2,5 +2,22 @@ import pandas
 
 
 class AnaliseDados:
-    def __init__(self):
-        ...
+    def __init__(self, dados_entrada):
+        self._dados_entrada = dados_entrada
+
+    def _view_dados(self):
+        return self._dados_entrada
+
+
+if __name__ == '__main__':
+    dados_teste_velocidade = {
+        'data_teste': '',
+        'teste_download': '',
+        'teste_upload': '',
+        'tempo_resposta': '',
+        'dados_cliente': ['', ''],
+    }
+
+    iniciando_obj_analise_dados = AnaliseDados(dados_teste_velocidade)
+    print(iniciando_obj_analise_dados._view_dados)
+
