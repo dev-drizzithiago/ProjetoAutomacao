96bce0fa652e472f7537f11abd2cd256631ad958
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-24 19:16:43 -0300

    Update main.py

--- teste_velocidade_v1/main.py
+++ teste_velocidade_v1/main.py
@@ -31,7 +31,7 @@ while True:
 
         # Criando data.csv
         dados_Data_Frame = analise_dados.AnaliseDados(dados_teste_velocidade).create_dataframe()
-        dados_Data_Frame.to_csv('data')
+        dados_Data_Frame.to_csv('data.csv')
 
         # lendo data.csv
         leitura_dados = analise_dados.AnaliseDados.view_dados_('data')
