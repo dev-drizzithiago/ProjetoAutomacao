e0e681a8e4e13f8459aea1f85fe5f840c0ab1350
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-28 11:59:28 -0300

    Update main.py

--- mikrotik_monitoramento/main.py
+++ mikrotik_monitoramento/main.py
@@ -2,7 +2,9 @@
 import mikrotik_logs
 
 if __name__ == '__main__':
+
     iniciando_obj = mikrotik_logs.BuscandoLogsMikrotik()
     logs = iniciando_obj.log_dhcp()
+
     for log in logs:
         print(log)
