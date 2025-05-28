2a26d5e3016a3ed06a723cb0a2240c8c79a9ad96
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-28 11:37:03 -0300

    Update main.py

--- mikrotik_monitoramento/main.py
+++ mikrotik_monitoramento/main.py
@@ -1,2 +1,12 @@
+from librouteros import connect
 from dotenv import load_dotenv
+import os
 load_dotenv()
+
+
+api_mikrotik = connect(
+    username=str(os.getenv('USERNAME')),
+    password=str(os.getenv('PASSWORD')),
+    host=str(os.getenv('HOST_FW')), # IP do seu MikroTik
+    port=str(os.getenv('PORT_FW')), # Porta padrão da API
+)
