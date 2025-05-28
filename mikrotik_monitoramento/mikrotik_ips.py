3071619152e063eaea53f9b9e7608ac06e29849c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-28 14:34:55 -0300

    Update mikrotik_ips.py

--- mikrotik_monitoramento/mikrotik_ips.py
+++ mikrotik_monitoramento/mikrotik_ips.py
@@ -9,8 +9,11 @@ class InfoEndIp:
         self.conexao_firewall = mikrotik_logs.BuscandoLogsMikrotik()
         fw_conectado = self.conexao_firewall.conexao_fw()
         leases = fw_conectado.path('ip', 'dhcp-server', 'lease')
+        active_leases = [lease for lease in leases if lease.get('status') == ['bound']]
 
-        return leases
+        return active_leases
 
 if __name__ == "__main__":
-    ...
+    iniciando_obj_ip = InfoEndIp()
+    ip_activate = iniciando_obj_ip.lease_ativas()
+    print(len(ip_activate))
