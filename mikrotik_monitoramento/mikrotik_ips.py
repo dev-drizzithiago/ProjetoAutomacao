dd0c0b6cd5dbd967687c5f648a6f23352cc23587
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-29 15:40:22 -0300

    Update mikrotik_ips.py

--- mikrotik_monitoramento/mikrotik_ips.py
+++ mikrotik_monitoramento/mikrotik_ips.py
@@ -1,18 +1,11 @@
-import mikrotik_logs
-
 
 class InfoEndIp:
     def __init__(self, obj_principal):
         self.obj_conexao_fw = obj_principal
 
     def lease_ativas(self):
-        
+
         leases = self.obj_conexao_fw.path('ip', 'dhcp-server', 'lease')
-        active_leases = [lease for lease in leases if lease.get('status') == ['bound']]
+        active_leases = [lease for lease in leases if lease.get('status') == 'bound']
 
-        return active_leases
-
-if __name__ == "__main__":
-    iniciando_obj_ip = InfoEndIp()
-    ip_activate = iniciando_obj_ip.lease_ativas()
-    print(len(ip_activate))
+        print(len(active_leases))
