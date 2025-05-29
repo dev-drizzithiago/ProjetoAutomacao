22252da59edca23de6e2e9ad16c85f774ae7d0a7
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-29 16:21:36 -0300

    Update mikrotik_logs.py

--- mikrotik_monitoramento/mikrotik_logs.py
+++ mikrotik_monitoramento/mikrotik_logs.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
+
 
 
 class BuscandoLogsMikrotik:
@@ -9,6 +11,7 @@ class BuscandoLogsMikrotik:
         self.lista_desatribuicao_ip = list()
 
         self._logs = None
+        self.__data_stamp = None
 
     def log_dhcp(self):
         self._logs = self.obj_conexao_fw.path('log')
@@ -19,6 +22,11 @@ class BuscandoLogsMikrotik:
 
     def log_conxao_bkp_internet(self):
         ...
+
+    def conversao_data_timestamp(self, data_fw):
+        data_com_fuso = datetime.strptime("%Y-%m-%d %H:%M:%S").replace(tzinfo=ZoneInfo("America/Sao_Paulo"))
+        self.__data_stamp = data_com_fuso.timestamp()
+        return data_stamp
 
     def analise_de_logs(self):
         logs = self._logs
@@ -35,13 +43,14 @@ class BuscandoLogsMikrotik:
                 if 'defconf assigned' in log[chaves_logs['4']]:
                     mac = str(log[chaves_logs['4']]).split('for')[-1].strip()
                     divisao_itens = mac.split(' ')
-                    horario_unix = datetime.timestamp(str(log[chaves_logs['2']]))
+
+                    horario_timestamp = self.conversao_data_timestamp(log[chaves_logs['2']])
 
                     if len(divisao_itens) == 2:
                         host_name = divisao_itens[-1]
 
                         if 'note' in divisao_itens[-1]:
-                            print(f'A: {horario_unix} - {host_name}')
+                            print(f'A: {horario_timestamp} - {host_name}')
 
                     self.lista_atribuicao_ip.append(mac)
 
