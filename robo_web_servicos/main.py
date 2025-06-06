6fce55b6611e7db656c022bcc252ca0896ff420c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-06 09:44:26 -0300

    Update main.py

--- robo_web_servicos/main.py
+++ robo_web_servicos/main.py
@@ -1,4 +1,9 @@
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -16,14 +21,42 @@ class RoboSites:
         self.site_acesso = site
 
 
+        DRIVE_CHROME.get(dados_entrada)
+
+        elemento_login = DRIVE_CHROME.find_element(By.ID, "email")
+        elemento_senha = DRIVE_CHROME.find_element(By.XPATH, "//input[@id='password']")
+
+        elemento_login.send_keys(os.getenv('EMAIL_ACESSO', ''))
+        sleep(0.5)
+        elemento_senha.send_keys(os.getenv('SENHA_ACESSO', ''))
+        elemento_senha.send_keys(Keys.ENTER)
+        sleep(6)
+        elemento_configuracao = DRIVE_CHROME.find_element(By.XPATH, "//svg[@xmlns='http://www.w3.org/2000/svg']")
+        elemento_configuracao.send_keys(Keys.ENTER)
+
+        sleep(10)
+
+
+
+
+ROOT_FOLDER = Path(__file__).parent
+PATH_CHROME_DRIVER = str(Path(ROOT_FOLDER / 'driver_google' / 'chromedriver.exe')).replace('\\', '/')
+DRIVE_CHROME = webdriver.Chrome()
+CHROME_SERVICE = Service(executable_path=str(PATH_CHROME_DRIVER))
+
 if __name__ == "__main__":
+
     lista_dados_acesso = {
-        'site': '',
-        'user_acesso': '',
-        'pass_acesso': '',
-        'email_usuario': '',
+        'url_site': '',
+        'user_acesso_site': '',
+        'pass_acesso_site': '',
+        'email_usuario_pessoal': '',
+        'email_usuario_empresa': '',
+        'usuario_login': '',
         'senha_usuario': '',
-        'cpf': '',
+        'cpf_usuario': '',
+        'data_nasc_usuario': '',
+        'data_admissao_usario': '',
     }
 
     inicio_obj = RoboSites()
