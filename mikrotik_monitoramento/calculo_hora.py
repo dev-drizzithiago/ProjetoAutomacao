4245a2cb6c1cd8b7e714e6b50febca587ac9f8d8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 14:27:25 -0300

    Update calculo_hora.py

--- mikrotik_monitoramento/calculo_hora.py
+++ mikrotik_monitoramento/calculo_hora.py
@@ -5,27 +5,39 @@ class CalculaHora:
     DATA_10_MIN_ATRAS: datetime
     date_now = datetime.now()
     def __init__(self):
-        self.UNIX_DATA_NOW = None
+        self.UNIX_DATA_DIA = None
         self.UNIX_DATE_FW = None
         self.horario_log = None
         self.DATA_HORA_INICIO_FORMAT = None
         self.DATA_HORA_STAMP = None
 
     def horario_inicio_trabalho_(self):
+        # Pega apenas a data, sem o horário
         data_atual = datetime.now().strftime('%Y-%m-%d')
+
+        # Fixei um horario para o inicio do expediente.
         horario_inicio = '07:00:00'
+
+        # junto a data atual com o horário fixado.
         data_hora_inicio = f'{data_atual} {horario_inicio}'
+
+        # Formatado a data e hora para ficar com o padrão do datetime
         format_data_hora = datetime.strptime(data_hora_inicio, '%Y-%m-%d %H:%M:%S')
+
+        # Transformo a data e hora em stamp, data formatada do UNIX.
         self.DATA_HORA_STAMP = datetime.timestamp(format_data_hora)
 
+        # print para analisar o resultado. 
         print(self.DATA_HORA_STAMP)
 
     def converter_hora_atual_stamp(self):
         self.horario_inicio_trabalho_()
+        SOBRA_CALCULO = int(self.date_now.timestamp()) - int(self.DATA_HORA_STAMP)
+        self.UNIX_DATA_DIA =  int(self.date_now.timestamp()) - SOBRA_CALCULO# resultado não bate, verificar
+        print(self.UNIX_DATA_DIA)
 
-        self.UNIX_DATA_NOW = int(self.date_now.timestamp()) - int(self.DATA_HORA_STAMP) # resultado não bate, verificar
-        print(self.UNIX_DATA_NOW)
-        self.DATA_HORA_INICIO_FORMAT = datetime.fromtimestamp(self.UNIX_DATA_NOW) # hora formatada para views
+        self.DATA_HORA_INICIO_FORMAT = datetime.fromtimestamp(self.UNIX_DATA_DIA) # hora formatada para views
+
         print(self.DATA_HORA_INICIO_FORMAT)
 
     def converter_hora_log_stamp(self, entrada_hora_log):
@@ -35,12 +47,12 @@ class CalculaHora:
         self.UNIX_DATE_FW = datetime.timestamp(CONVERSAO_DATA)
 
     def comparacao_data_atual_x_log(self):
-        if self.UNIX_DATE_FW > self.UNIX_DATA_NOW:
+        if self.UNIX_DATE_FW >= self.UNIX_DATA_DIA:
             return True
 
 
 if __name__ == "__main__":
     obj_inicio = CalculaHora()
     obj_inicio.horario_inicio_trabalho_()
-    obj_inicio.converter_hora_log_stamp('2025-06-12 11:35:56')
+    obj_inicio.converter_hora_log_stamp('2025-06-12 07:00:00')
     print(obj_inicio.comparacao_data_atual_x_log())
