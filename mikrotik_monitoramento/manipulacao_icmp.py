4fedc1a807960cf28ed9989a1e2c97650618603f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 16:19:04 -0300

    Update manipulacao_icmp.py

--- mikrotik_monitoramento/manipulacao_icmp.py
+++ mikrotik_monitoramento/manipulacao_icmp.py
@@ -12,6 +12,10 @@ class ManipulacaoIcmpHosts:
         ping_result = subprocess.run(
             [
             'ping', '-c', '1', self.ip_address
-            ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+            ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True
         )
+        print(ping_result.returncode)
 
+
+obj_ping = ManipulacaoIcmpHosts()
+obj_ping.ping_icmp_redeLocal('192.168.0.10')
