78aeb9d8e23b380a20661ac9f1f35026fb3e2c1b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 10:57:18 -0300

    Update calculo_hora.py

--- mikrotik_monitoramento/calculo_hora.py
+++ mikrotik_monitoramento/calculo_hora.py
@@ -9,9 +9,9 @@ class CalculaHora:
 
     def _converter_hora_atual_stamp(self):
         print('Convertendo horário atual para UNIX')
-        self.UNIX_DATA_NOW = int(self.date_now.timestamp()) - 300
-        self.DATA_5_MIN_ATRAS = datetime.fromtimestamp(self.UNIX_DATA_NOW) # hora formatada
-
+        self.UNIX_DATA_NOW = int(self.date_now.timestamp()) - 600
+        self.DATA_10_MIN_ATRAS = datetime.fromtimestamp(self.UNIX_DATA_NOW) # hora formatada
+        print(self.DATA_10_MIN_ATRAS)
 
     def _converter_hora_log_stamp(self, entrada_hora_log):
         print('Convertendo horário log para UNIX')
@@ -22,12 +22,12 @@ class CalculaHora:
     def _comparacao_data_atual_x_log(self):
         print('Comparando horário ATUAL com o horário de LOG')
         if self.UNIX_DATE_FW > self.UNIX_DATA_NOW:
-            print('Comparação realizada')
+            return True
+
 
 
 if __name__ == "__main__":
     obj_inicio = CalculaHora()
     obj_inicio._converter_hora_atual_stamp()
-    obj_inicio._converter_hora_log_stamp('2025-06-12 09:18:17')
+    obj_inicio._converter_hora_log_stamp('2025-06-12 10:50:56')
     obj_inicio._comparacao_data_atual_x_log()
-
