c59dc74c6db31a0bc8ffeacc31d58a4213648394
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 11:43:16 -0300

    Update calculo_hora.py

--- mikrotik_monitoramento/calculo_hora.py
+++ mikrotik_monitoramento/calculo_hora.py
@@ -4,34 +4,32 @@ from datetime import datetime
 class CalculaHora:
     date_now = datetime.now()
     def __init__(self):
+        self.UNIX_DATE_FW = None
         self.horario_log = None
         self.DATA_5_MIN_ATRAS = None
 
-    def _converter_hora_atual_stamp(self):
+    def converter_hora_atual_stamp(self):
         print('- Convertendo horário atual para UNIX...')
         self.UNIX_DATA_NOW = int(self.date_now.timestamp()) - 600
         self.DATA_10_MIN_ATRAS = datetime.fromtimestamp(self.UNIX_DATA_NOW) # hora formatada
         print(self.DATA_10_MIN_ATRAS)
 
-    def _converter_hora_log_stamp(self, entrada_hora_log):
-        self._converter_hora_atual_stamp()
+    def converter_hora_log_stamp(self, entrada_hora_log):
+        self.converter_hora_atual_stamp()
         print('- Convertendo horário log para UNIX...')
         self.horario_log = entrada_hora_log
         CONVERSAO_DATA = datetime.strptime(self.horario_log, '%Y-%m-%d %H:%M:%S')
         self.UNIX_DATE_FW = datetime.timestamp(CONVERSAO_DATA)
 
-    def _comparacao_data_atual_x_log(self):
+    def comparacao_data_atual_x_log(self):
         print('- Comparando horário ATUAL com o horário de LOG...')
 
         if self.UNIX_DATE_FW > self.UNIX_DATA_NOW:
-            print("Data LOG no intervalo de busca")
+            # print("Data LOG no intervalo de busca")
             return self.UNIX_DATE_FW
-        else:
-            print('Não pegamos logs abaixo de 10 minutos do horário atual. ')
-
 
 
 if __name__ == "__main__":
     obj_inicio = CalculaHora()
-    obj_inicio._converter_hora_log_stamp('2025-06-12 11:35:56')
-    obj_inicio._comparacao_data_atual_x_log()
+    obj_inicio.converter_hora_log_stamp('2025-06-12 11:35:56')
+    obj_inicio.comparacao_data_atual_x_log()
