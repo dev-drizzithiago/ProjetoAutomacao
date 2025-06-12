e9d82b29460841cea185acac858cf3701c3778e8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 10:14:04 -0300

    Update calculo_hora.py

--- mikrotik_monitoramento/calculo_hora.py
+++ mikrotik_monitoramento/calculo_hora.py
@@ -1,17 +1,20 @@
-
-
 from datetime import datetime
 
-date_now = datetime.now()
-UNIX_DATA_NOW = int(date_now.timestamp()) - 300
-DATA_5_MIN_ATRAS = datetime.fromtimestamp(UNIX_DATA_NOW)
 
+class CalculaHora:
+    def __init__(self):
+        self.horario_log = None
 
-DATA =  '2025-06-12 09:18:17'
-CONVERSAO_DATA = datetime.strptime(DATA, '%Y-%m-%d %H:%M:%S')
-UNIX_DATE_FW = datetime.timestamp(CONVERSAO_DATA)
+    def _converter_hora_atual_stamp(self):
+        date_now = datetime.now()
+        UNIX_DATA_NOW = int(date_now.timestamp()) - 300
+        DATA_5_MIN_ATRAS = datetime.fromtimestamp(UNIX_DATA_NOW)
 
+    def _converter_hora_log_stamp(self, entrada_hora_log):
+        self.horario_log =  '2025-06-12 09:18:17'
+        CONVERSAO_DATA = datetime.strptime(DATA, '%Y-%m-%d %H:%M:%S')
+        UNIX_DATE_FW = datetime.timestamp(CONVERSAO_DATA)
 
-if UNIX_DATE_FW > UNIX_DATA_NOW:
-    print(UNIX_DATE_FW)
-    print(UNIX_DATA_NOW)
+        if UNIX_DATE_FW > UNIX_DATA_NOW:
+            print(UNIX_DATE_FW)
+            print(UNIX_DATA_NOW)
