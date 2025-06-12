8b1647996f1d0a229dca26c00f00f66af26777cf
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 13:24:35 -0300

    Update calculo_hora.py

--- mikrotik_monitoramento/calculo_hora.py
+++ mikrotik_monitoramento/calculo_hora.py
@@ -8,11 +8,24 @@ class CalculaHora:
         self.UNIX_DATA_NOW = None
         self.UNIX_DATE_FW = None
         self.horario_log = None
-        self.DATA_10_MIN_ATRAS = None
+        self.DATA_HORA_INICIO_FORMAT = None
+        self.DATA_HORA_STAMP = None
+
+    def horario_inicio_trabalho_(self):
+        data_atual = datetime.now().strftime('%Y-%m-%d')
+        horario_inicio = '07:00:00'
+        data_hora_inicio = f'{data_atual} {horario_inicio}'
+        format_data_hora = datetime.strptime(data_hora_inicio, '%Y-%m-%d %H:%M:%S')
+        self.DATA_HORA_STAMP = datetime.timestamp(format_data_hora)
+
+        print(self.DATA_HORA_STAMP)
 
     def converter_hora_atual_stamp(self):
-        self.UNIX_DATA_NOW = int(self.date_now.timestamp()) - 600
-        self.DATA_10_MIN_ATRAS = datetime.fromtimestamp(self.UNIX_DATA_NOW) # hora formatada
+        self.horario_inicio_trabalho_()
+
+        self.UNIX_DATA_NOW = int(self.date_now.timestamp()) - int(self.DATA_HORA_STAMP) # resultado não bate, verificar
+        self.DATA_HORA_INICIO_FORMAT = datetime.fromtimestamp(self.UNIX_DATA_NOW) # hora formatada para views
+        print(self.DATA_HORA_INICIO_FORMAT)
 
     def converter_hora_log_stamp(self, entrada_hora_log):
         self.converter_hora_atual_stamp()
@@ -22,10 +35,11 @@ class CalculaHora:
 
     def comparacao_data_atual_x_log(self):
         if self.UNIX_DATE_FW > self.UNIX_DATA_NOW:
-            return self.UNIX_DATE_FW
+            return True
 
 
 if __name__ == "__main__":
     obj_inicio = CalculaHora()
+    obj_inicio.horario_inicio_trabalho_()
     obj_inicio.converter_hora_log_stamp('2025-06-12 11:35:56')
     print(obj_inicio.comparacao_data_atual_x_log())
