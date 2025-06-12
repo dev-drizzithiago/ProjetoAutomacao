018770777b8d4cd11df088bf80c470959e07b776
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 16:15:01 -0300

    Update manipulacao_icmp.py

--- mikrotik_monitoramento/manipulacao_icmp.py
+++ mikrotik_monitoramento/manipulacao_icmp.py
@@ -1,8 +1,17 @@
 
+import subprocess
+import socket
 
 class ManipulacaoIcmpHosts:
     def __init__(self):
+        self.ip_address = None
         ...
 
-    def ping_icmp_redeLocal(self):
-        ...
+    def ping_icmp_redeLocal(self, endereco_ip):
+        self.ip_address = endereco_ip
+        ping_result = subprocess.run(
+            [
+            'ping', '-c', '1', self.ip_address
+            ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+
