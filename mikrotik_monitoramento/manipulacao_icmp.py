5d83f4c3b00b614193da96100e28550dcf44213b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-12 17:20:55 -0300

    Update manipulacao_icmp.py

--- mikrotik_monitoramento/manipulacao_icmp.py
+++ mikrotik_monitoramento/manipulacao_icmp.py
@@ -3,27 +3,36 @@ import subprocess
 import socket
 
 class ManipulacaoIcmpHosts:
+
+    LISTA_PING_ON = list()
+    LISTA_PING_OFF = list()
+    LISTA_HOSTNAME = list()
+
+
     def __init__(self):
         self.ip_address = None
         ...
 
     def ping_icmp_redeLocal(self, endereco_ip):
-        print(f'Processando icmp com o endereço ip 192.168.0.{endereco_ip}')
 
         ping_result = subprocess.run(
             'ping ' + f'{endereco_ip} ' + '-n 1 -w 1 ', stdout=subprocess.PIPE, text=True
         )
 
         if ping_result.returncode == 0:
+            self.LISTA_PING_ON.append(endereco_ip)
             self.buscando_host(endereco_ip)
+        else:
+            self.LISTA_PING_OFF.append(endereco_ip)
 
     def buscando_host(self, endereco_ip):
         print('Processando hostname...')
         try:
             host_name = socket.gethostbyaddr(endereco_ip)
-            print(host_name)
+            self.LISTA_HOSTNAME.append(host_name)
         except socket.herror:
-            print(f'Não foi encontrado o hostname dessa do endereço de ip {endereco_ip}')
+            ...
+            # print(f'Não foi encontrado o hostname dessa do endereço de ip {endereco_ip}')
 
 
 obj_ping = ManipulacaoIcmpHosts()
@@ -34,3 +43,9 @@ while True:
     range_end_ip += 1
     if range_end_ip == 254:
         break
+
+for item in obj_ping.LISTA_PING_ON:
+    print(f'Lista de IPs Ativos: {item}')
+
+for item in obj_ping.LISTA_HOSTNAME:
+    print(f'Lista de HostNames: {item}')
