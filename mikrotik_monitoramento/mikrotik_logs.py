19a1b10dd062ce376d70c83d7aec0e133fb5784b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-13 11:38:09 -0300

    Update mikrotik_logs.py

--- mikrotik_monitoramento/mikrotik_logs.py
+++ mikrotik_monitoramento/mikrotik_logs.py
@@ -57,9 +57,16 @@ class BuscandoLogsMikrotik:
                 if 'defconf assigned' in log[chaves_logs['4']]:
                     info_log_on = f'{hora_do_log} - {log[chaves_logs['4']]}'
                     self.lista_atribuicao_ip.append(info_log_on)
-                    print()
                 contador += 1
 
                 if 'defconf deassigned' in log[chaves_logs['4']]:
                     info_log_off = f'{hora_do_log} - {log[chaves_logs['4']]}'
                     self.lista_desatribuicao_ip.append(info_log_off)
+
+
+            for item in self.lista_atribuicao_ip:
+                if item not in self.lista_desatribuicao_ip:
+                    self.lista_ip_on.append(item)
+
+
+            print(self.lista_ip_on)
