69ddeea596b8cb8e12b4233769261b2e348f5710
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-13 08:44:38 -0300

    Update manipulacao_icmp.py

--- mikrotik_monitoramento/manipulacao_icmp.py
+++ mikrotik_monitoramento/manipulacao_icmp.py
@@ -14,27 +14,30 @@ class ManipulacaoIcmpHosts:
         ...
 
     def ping_icmp_redeLocal(self, endereco_rede):
-        verificando_rede = int(endereco_rede.split('.')[-1])
-        if verificando_rede > 0:
+        verificando_host_rede = int(endereco_rede.split('.')[-1])
+
+        if verificando_host_rede > 0:
             print('Rede invalida.')
         else:
             endereco_ip = endereco_rede
-            
-        while True:
-            ping_result = subprocess.run(
-                'ping ' + f'{endereco_ip} ' + '-n 1 -w 1 ', stdout=subprocess.PIPE, text=True
-            )
 
-            if ping_result.returncode == 0:
-                self.LISTA_PING_ON.append(endereco_ip)
-                self.buscando_host(endereco_ip)
-            else:
-                self.LISTA_PING_OFF.append(endereco_ip)
+        print(verificando_rede)
 
-            if host == 254:
-                break
-
-        return {'LISTA_HOSTNAME': self.LISTA_HOSTNAME,'LISTA_PING_ON': self.LISTA_PING_ON,}
+        # while True:
+        #     ping_result = subprocess.run(
+        #         'ping ' + f'{endereco_ip} ' + '-n 1 -w 1 ', stdout=subprocess.PIPE, text=True
+        #     )
+        #
+        #     if ping_result.returncode == 0:
+        #         self.LISTA_PING_ON.append(endereco_ip)
+        #         self.buscando_host(endereco_ip)
+        #     else:
+        #         self.LISTA_PING_OFF.append(endereco_ip)
+        #
+        #     if host == 254:
+        #         break
+        #
+        # return {'LISTA_HOSTNAME': self.LISTA_HOSTNAME,'LISTA_PING_ON': self.LISTA_PING_ON,}
 
     def buscando_host(self, endereco_ip):
         try:
@@ -51,4 +54,3 @@ if __name__ == '__main__':
 
     rede_local = '192.168.0.0'
     obj_ping.ping_icmp_redeLocal(rede_local)
-    range_end_ip += 1
