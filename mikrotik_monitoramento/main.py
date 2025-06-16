23a0c31bd7f9b113ddfdc0d580947aa66177d1c5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-16 17:16:54 -0300

    Update main.py

--- mikrotik_monitoramento/main.py
+++ mikrotik_monitoramento/main.py
@@ -47,16 +47,16 @@ if __name__ == '__main__':
     else:
         while True:
             print('Processando...')
-            obj_logs = mikrotik_logs.BuscandoLogsMikrotik(conexao_fw)
-            obj_logs.log_dhcp(None)
-            result_ip = obj_logs.analise_de_logs()
-            # print(result_ip)
-            # print('Quantidades de ip: ', len(result_ip))
-
-
-            obj_info_ip = mikrotik_ips.InfoEndIp(conexao_fw)
-            quantidade_clientes_dhcp = obj_info_ip.lease_ativas()
-            print(quantidade_clientes_dhcp)
+            # obj_logs = mikrotik_logs.BuscandoLogsMikrotik(conexao_fw)
+            # obj_logs.log_dhcp(None)
+            # result_ip = obj_logs.analise_de_logs()
+            # # print(result_ip)
+            # # print('Quantidades de ip: ', len(result_ip))
+            #
+            #
+            # obj_info_ip = mikrotik_ips.InfoEndIp(conexao_fw)
+            # quantidade_clientes_dhcp = obj_info_ip.lease_ativas()
+            # print(quantidade_clientes_dhcp)
 
             # obj_icmp = manipulacao_icmp.ManipulacaoIcmpHosts()
             # result = obj_icmp.ping_icmp_redeLocal(result_ip)
