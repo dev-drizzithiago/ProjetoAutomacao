83be8877f0bafd0a2286c28f7ef024558e533607
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-16 15:12:09 -0300

    Update manipulacao_icmp.py

--- mikrotik_monitoramento/manipulacao_icmp.py
+++ mikrotik_monitoramento/manipulacao_icmp.py
@@ -14,12 +14,6 @@ class ManipulacaoIcmpHosts:
         ...
 
     def ping_icmp_redeLocal(self, list_ip_hosts):
-        verificando_host_rede = int(endereco_rede.split('.')[-1])
-        prefixo_rede_ = endereco_rede.split('.')[:-1]
-
-        if verificando_host_rede > 0:
-            print('Rede invalida.')
-            return
 
         print('Processando...')
         for ip_host_ in list_ip_hosts:
@@ -29,42 +23,35 @@ class ManipulacaoIcmpHosts:
             )
 
             if ping_result.returncode == 0:
-                self.LISTA_PING_ON.append(endereco_ip)
-                self.buscando_host(endereco_ip)
+                self.LISTA_PING_ON.append(ip_host_)
+                self.buscando_host(ip_host_)
             else:
-                self.LISTA_PING_OFF.append(endereco_ip)
-            self.HOST += 1
-            if self.HOST == 254:
-                break
+                self.LISTA_PING_OFF.append(ip_host_)
 
         return {'LISTA_HOSTNAME': self.LISTA_HOSTNAME,'LISTA_PING_ON': self.LISTA_PING_ON,}
 
-    def buscando_host(self, endereco_ip):
+    def buscando_host(self, ip_host_):
         try:
-            host_name = socket.gethostbyaddr(endereco_ip)
+            host_name = socket.gethostbyaddr(ip_host_)
             self.LISTA_HOSTNAME.append(host_name[0])
         except socket.herror:
             ...
-            # print(f'Não foi encontrado o hostname dessa do endereço de ip {endereco_ip}')
-
-
-lista_teste_ip = [
-]
 
 obj_ping = ManipulacaoIcmpHosts()
 
 
 if __name__ == '__main__':
-    while True:
-        entrada_rede_base = input('Digital uma rede base (192.168.0.0): ')
-        result = obj_ping.ping_icmp_redeLocal(entrada_rede_base)
+    lista_end_hosts = '192.168.0.250', '192.168.0.10'
+    result = obj_ping.ping_icmp_redeLocal(lista_end_hosts)
 
-        if not result:
-            print('Digital uma rede Base...!')
+    if not result:
+        print('Digital uma rede Base...!')
 
-        nome_host = result['LISTA_HOSTNAME']
-        ip_host = result['LISTA_PING_ON']
+    nome_host = result['LISTA_HOSTNAME']
+    ip_host = result['LISTA_PING_ON']
 
-        for item in nome_host:
-            print(item)
+    for item in nome_host:
+        print(item)
 
+    for item in ip_host:
+        print(item)
