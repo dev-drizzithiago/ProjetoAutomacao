8f443c63d26c1f115eb164f2c8c630e496c518b9
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-06-17 14:57:44 -0300

    Update servicos_rede.py

--- mikrotik_monitoramento/servicos_rede.py
+++ mikrotik_monitoramento/servicos_rede.py
@@ -1,1 +1,20 @@
 from zeroconf import ServiceBrowser, ServiceListener, Zeroconf
+
+servico = "_services._dns-sd._udp.local."
+
+class DescobertaRede(ServiceListener):
+    def adicionar_servicos(self, zeroconf, tipo_servico, nome):
+        info = zeroconf.get_service_info(tipo_servico, nome)
+        if info:
+            print(f'Servicos encontrados: {nome}')
+
+    def remove_servicos(self, zeroconf, tipo, nome):
+        print(f'[-] Servico removido: {nome}')
+
+    def atualiza_servico(self, zeroconf, tipo, nome):
+        print(f'[-] Servico atualizado: {nome}')
+
+zeroconf = Zeroconf()
+listener = DescobertaRede()
+
+Browser = ServiceBrowser(zeroconf, servico, listener)
