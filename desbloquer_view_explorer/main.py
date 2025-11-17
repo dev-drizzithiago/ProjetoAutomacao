1c6f5b155c9fe5ceb5fd143f1b3298aa1f0bd92a
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-17 10:58:05 -0300

    Update main.py

--- desbloquer_view_explorer/main.py
+++ desbloquer_view_explorer/main.py
@@ -1,4 +1,24 @@
-import os
+"""
+Detalhamento do Comando
+Este comando combina duas ações essenciais, separadas por um ponto e vírgula (;):
+
+taskkill /f /im explorer.exe:
+
+taskkill: É o utilitário do Windows usado para encerrar processos em execução.
+
+/f: Força o encerramento do processo (fundamental para garantir que o processo seja finalizado imediatamente).
+
+/im explorer.exe: Especifica a imagem do processo a ser encerrado, que é o explorer.exe (o Explorador de Arquivos/Shell do Windows).
+
+Start-Process explorer.exe:
+
+Start-Process: É o cmdlet do PowerShell usado para iniciar um novo processo.
+
+explorer.exe: Inicia uma nova instância do Explorador de Arquivos.
+"""
+
+
+
 from pathlib import Path
 from subprocess import run, CalledProcessError
 from time import sleep
@@ -16,14 +36,15 @@ try:
         check=True
     )
 
-    sleep(2)
     print('Reiniciando o Windows Explorer.exe para aplicar as mudanças')
+    sleep(5)
     run(
         ['powershell', '-Command', comando_powershell_reiniciar_explorer],
         shell=True,
         capture_output=True,
         check=True
     )
+    print("Windows Explorer reiniciado. Verifique agora o Painel de Visualização.")
 
 except CalledProcessError as error:
     print(f'\n Erro ao executar o PowerShell: ')
