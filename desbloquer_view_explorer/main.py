0c485345526c569db84b62b38fe7dc20b4feaae8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-17 10:54:02 -0300

    Update main.py

--- desbloquer_view_explorer/main.py
+++ desbloquer_view_explorer/main.py
@@ -1,10 +1,37 @@
-
-import re
+import os
 from pathlib import Path
+from subprocess import run, CalledProcessError
+from time import sleep
 
 home_usuario = Path.home()
 
-comando_powershell = "Get-ChildItem -Path . -Recurse | Unblock-File "
+comando_powershell_desbloquear_MOTW = f"Get-ChildItem -Path {home_usuario} -Recurse | Unblock-File"
+comando_powershell_reiniciar_explorer = 'taskkill /f /im explorer.exe; Start-Process explorer.exe'
 
-result_comando = os.run()
+try:
+    result_shell = run(
+        ['powershell', "-Command", comando_powershell_desbloquear_MOTW],
+        shell=True,
+        capture_output=True,
+        check=True
+    )
+
+    sleep(2)
+    print('Reiniciando o Windows Explorer.exe para aplicar as mudanças')
+    run(
+        ['powershell', '-Command', comando_powershell_reiniciar_explorer],
+        shell=True,
+        capture_output=True,
+        check=True
+    )
+
+except CalledProcessError as error:
+    print(f'\n Erro ao executar o PowerShell: ')
+    print(f'Stdout: {error.stdout} ')
+    print(f'Stderr: {error.stderr} ')
+    print('Verifique se o perfil foi executado como administrador.')
+except Exception as error:
+    print('Ocorreu um erro inesperado:', error)
+
+
 
