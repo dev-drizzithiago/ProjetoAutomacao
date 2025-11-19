7c5d077aa9adccc6e488a5e92dbf411daa7e35b5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-19 10:34:43 -0300

    Create main.py

---
+++ utilitarios_segeti/main.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+if __name__ == "__main__":
+    while True:
+        print(
+            """
+            [ 1 ] Adicionar Redes Wifi.
+            [ 2 ] Configurar adaptadores de rede.
+            [ 3 ] Desbloquear Visualização no windows.
+            [ 0 ] Sair. 
+            """
+        )
+        print('---' * 30)
+        try:
+            opc = int(input('Escolha uma opção: '))
+
+            if opc == 1:
+                ...
+            elif opc == 2:
+                ...
+            elif opc == 3:
+                ...
+            elif opc == 0:
+                print('---' * 30)
+                print('Saindo do programa')
+                sleep(2)
+            else:
+                print('Opção não existe.')
+
+        except ValueError:
+            print()
+            print('---' * 30)
+            print('Valor incorreto')
+
+
