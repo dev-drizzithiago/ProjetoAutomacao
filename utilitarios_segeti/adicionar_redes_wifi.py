167b43ca5e07fd3ded6bc82ab6fdb03609f536cd
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-11-24 09:11:47 -0300

    Update adicionar_redes_wifi.py

--- utilitarios_segeti/adicionar_redes_wifi.py
+++ utilitarios_segeti/adicionar_redes_wifi.py
@@ -24,92 +24,95 @@ try:
 except FileExistsError:
     ...
 
+class RedesWifi:
+    def __init__(self):
+        self.inicio = None
 
-def adicionando_rede_wifi():
-    """
-    Função responsável em adicionar as redes que estão disponivel para os funcionários da segeti.
-    :param: perfil_xml - Cria um arquivo xml para que o powershel identifique os parâmtros da rede.
-    :return:
-    """
-
-    # Serão adicionadas a quantidade de rede que esta na lista.
-    for rede in redes_disponiveis:
-
-        perfil_xml = f"""
-        <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
-            <name>{rede['nome']}</name>
-            <SSIDConfig>
-                <SSID>
-                    <name>{rede['nome']}</name>
-                </SSID>
-            </SSIDConfig>
-            <connectionType>ESS</connectionType>
-            <connectionMode>auto</connectionMode>
-            <MSM>
-                <security>
-                    <authEncryption>
-                        <authentication>WPA2PSK</authentication>
-                        <encryption>AES</encryption>
-                        <useOneX>false</useOneX>
-                    </authEncryption>
-                    <sharedKey>
-                        <keyType>passPhrase</keyType>
-                        <protected>false</protected>
-                        <keyMaterial>{rede['senha']}</keyMaterial>
-                    </sharedKey>
-                </security>
-            </MSM>
-        </WLANProfile>
+    def adicionando_rede_wifi(self):
         """
+        Função responsável em adicionar as redes que estão disponivel para os funcionários da segeti.
+        :param: perfil_xml - Cria um arquivo xml para que o powershel identifique os parâmtros da rede.
+        :return:
+        """
+        # Serão adicionadas a quantidade de rede que esta na lista.
+        for rede in redes_disponiveis:
 
-        # Cria o arquivo completo do arquivo xml
-        caminho_completo_xml = rf'{local_xml_pc_local}\{arquivo_xml}'
+            perfil_xml = f"""
+            <WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+                <name>{rede['nome']}</name>
+                <SSIDConfig>
+                    <SSID>
+                        <name>{rede['nome']}</name>
+                    </SSID>
+                </SSIDConfig>
+                <connectionType>ESS</connectionType>
+                <connectionMode>auto</connectionMode>
+                <MSM>
+                    <security>
+                        <authEncryption>
+                            <authentication>WPA2PSK</authentication>
+                            <encryption>AES</encryption>
+                            <useOneX>false</useOneX>
+                        </authEncryption>
+                        <sharedKey>
+                            <keyType>passPhrase</keyType>
+                            <protected>false</protected>
+                            <keyMaterial>{rede['senha']}</keyMaterial>
+                        </sharedKey>
+                    </security>
+                </MSM>
+            </WLANProfile>
+            """
 
-        # Cria o arquivo xml para o powershell conseguir ler.
-        with open(caminho_completo_xml, "w") as arquivo:
-            arquivo.write(perfil_xml)
+            # Cria o arquivo completo do arquivo xml
+            caminho_completo_xml = rf'{local_xml_pc_local}\{arquivo_xml}'
 
-        comando_shell_add_wifi = f'netsh wlan add profile filename="{caminho_completo_xml}"'
+            # Cria o arquivo xml para o powershell conseguir ler.
+            with open(caminho_completo_xml, "w") as arquivo:
+                arquivo.write(perfil_xml)
 
-        try:
-            subprocess.run(['powershell', '-Command', comando_shell_add_wifi],
-                           capture_output=True, text=True, shell=True)
-            print(f' -Rede {rede['nome']} adicionada...')
+            comando_shell_add_wifi = f'netsh wlan add profile filename="{caminho_completo_xml}"'
 
-        except Exception as error:
-            print(f'Ocorreu erro na execusão do programa: {error}')
-            # Quando ocorrer algum erro, programa sera fechado.
-            return (f'Não foi possível adicionar a rede {rede['nome']} \n'
-                    f'Entre em contato com o desenvolvedor \n'
-                    f'th_grifon@hotmail.com')
+            try:
+                subprocess.run(['powershell', '-Command', comando_shell_add_wifi],
+                               capture_output=True, text=True, shell=True)
+                print(f' -Rede {rede['nome']} adicionada...')
 
-    # A confirmação só vai retorna se todas as redes forem adicionadas com sucesso.
-    # Apos as cofigurações,
+            except Exception as error:
+                print(f'Ocorreu erro na execusão do programa: {error}')
+                # Quando ocorrer algum erro, programa sera fechado.
+                return (f'Não foi possível adicionar a rede {rede['nome']} \n'
+                        f'Entre em contato com o desenvolvedor \n'
+                        f'th_grifon@hotmail.com')
 
-    os.remove(caminho_completo_xml)
-    sleep(1)
-    os.rmdir(local_xml_pc_local)
+        # A confirmação só vai retorna se todas as redes forem adicionadas com sucesso.
+        # Apos as cofigurações,
+
+        os.remove(caminho_completo_xml)
+        sleep(1)
+        os.rmdir(local_xml_pc_local)
+
+        print()
+        print("----" * 20)
+        return 'Redes adicionadas com sucesso \n'
+
+if __name__ == '__main__':
+    print()
+    print("----" * 20)
+    print('Programa desenvolvido por dev-drizzithiago')
 
     print()
     print("----" * 20)
-    return 'Redes adicionadas com sucesso \n'
+    print('Configurando suas redes, aguarde...! \n')
 
+    # Chama a função
+    iniciando_programa = RedesWifi()
+    iniciando_programa.adicionando_rede_wifi()
 
-print()
-print("----" * 20)
-print('Programa desenvolvido por dev-drizzithiago')
+    # Limpa todos os texto e informa apenas que a rede foi adicionada
+    os.system('cls')
 
-print()
-print("----" * 20)
-print('Configurando suas redes, aguarde...! \n')
-
-# Chama a função
-iniciando_programa = adicionando_rede_wifi()
-
-# Limpa todos os texto e informa apenas que a rede foi adicionada
-os.system('cls')
-
-# indica ao usuário que a rede foi adicionada.
-print('\n')
-print(iniciando_programa)
-sleep(10)
+    # indica ao usuário que a rede foi adicionada.
+    print('\n')
+    print(iniciando_programa)
+    sleep(10)
