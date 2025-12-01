5927af402d2fa8e204fe42cec5d8bdd3c81850f5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-01 13:35:51 -0300

    Update configurar_calendario.py

--- utilitarios_segeti/configurar_calendario.py
+++ utilitarios_segeti/configurar_calendario.py
@@ -1,4 +1,19 @@
 """
-Tem como finalidade compartilhar a caixa de correio da segeticonsultoria. 
+Tem como finalidade compartilhar a caixa de correio da segeticonsultoria.
 
 """
+
+class ShareCalendarMail:
+
+    def __init__(self):
+        pass
+
+    def adicionar_reviewer(self):
+        pass
+
+    def adicionar_(self):
+        pass
+
+
+if __name__ == '__main__':
+    obj_calendar = ShareCalendarMail()
