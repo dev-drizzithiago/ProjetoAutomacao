c0f1d61a8176f199d86748234de5a890d8ac72a1
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-02 13:09:41 -0300

    Update configurar_calendario.py

--- utilitarios_segeti/configurar_calendario.py
+++ utilitarios_segeti/configurar_calendario.py
@@ -5,15 +5,38 @@ Tem como finalidade compartilhar a caixa de correio da segeticonsultoria.
 
 class ShareCalendarMail:
 
-    def __init__(self):
+    def __init__(self, compartilhado, usuario):
+        pass
+        self.email_compartilhamento = compartilhado
+        self.email_usuario = usuario
+
+    def adicionar_AvailabilityOnly(self):
         pass
 
-    def adicionar_reviewer(self):
+    def adicionar_LimitedDetails(self):
         pass
 
-    def adicionar_(self):
+    def adicionar_Reviewer(self):
+        pass
+
+    def adicionar_Author(self):
+        pass
+
+    def adicionar_Editor(self):
+        pass
+
+    def adicionar_PublishingEditor(self):
+        pass
+
+    def adicionar_Owner(self):
         pass
 
 
 if __name__ == '__main__':
-    obj_calendar = ShareCalendarMail()
+    print()
+    print('Compartilhe um e-mail compartilhado')
+    print('---' * 25)
+    email_compartilhado = input('E-mail compartilhado: ')
+    email_usuario = input('E-mail do usuário: ')
+
+    obj_calendar = ShareCalendarMail(email_compartilhado, email_usuario)
