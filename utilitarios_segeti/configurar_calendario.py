7d1f37d9d2cd9c716083352d199582c6603ab1a4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-02 15:43:03 -0300

    Update configurar_calendario.py

--- utilitarios_segeti/configurar_calendario.py
+++ utilitarios_segeti/configurar_calendario.py
@@ -3,6 +3,9 @@ Tem como finalidade compartilhar a caixa de correio da segeticonsultoria.
 
 """
 import subprocess
+import itertools
+from time import sleep
+from threading import Event, Thread
 
 class ShareCalendarMail:
 
@@ -10,6 +13,38 @@ class ShareCalendarMail:
         pass
         self.email_compartilhamento = compartilhado
         self.email_usuario = usuario
+
+    def _run_processo_powershell(self, comando_shell):
+        resultado_processo = run(
+            ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", comando_shell],
+            shell=True,
+            check=False,
+            capture_output=True
+        )
+        return resultado_processo
+
+    def _spinner(self, stop_event, prefix='Processando... '):
+
+        ciclo = itertools.cycle(['|', '/', '-', '\\'])
+
+        while not stop_event.is_set():
+            # \r volta o cursor para início da linha
+            print(f"\r{prefix} {next(ciclo)}", end='', flush=True)
+            sleep(0.1)
+
+        # limpa linha ao finalizar
+        print('\n' + ' ' * 60 + '\r', end='', flush=True)
+
+    def _run_spinner(self, comando_str, texto_spinner):
+        stop_event = Event()
+        _thread = Thread(target=self._spinner, args=(stop_event, texto_spinner), daemon=True)
+        _thread.start()
+        try:
+            result = self._run_processo_powershell(comando_str)
+            return result
+        finally:
+            stop_event.set()
+            _thread.join()
 
     def adicionar_AvailabilityOnly(self):
         comando_shell = (
