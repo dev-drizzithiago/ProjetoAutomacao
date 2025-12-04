1f39e5e65c443207984ac896d8c4979d12c53dd4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-04 16:38:59 -0300

    Update main.py

--- leitura_pdf/main.py
+++ leitura_pdf/main.py
@@ -4,16 +4,17 @@ import pandas as pd
 class LeituraPdf:
     def __init__(self, arquivo_pdf):
         self.arquivo_pdf = arquivo_pdf
-        pass
+        self.lista_dados_pandas = []  # 1) lista com o texto de cada página
+        self.texto_completo = ''      # 2) texto concatenado de todas as páginas
+        self.tabela_campos = None     # 3) DataFrame com campos extraídos (opcional)
 
     def extraindo_texto(self):
-
         leitura = PdfReader(self.arquivo_pdf)
         for text_page in leitura.pages:
             texto = text_page.extract_text()
             print()
             print(texto)
-
+            print(type(texto))
 
 if __name__ == '__main__':
     caminho_arquivo = 'teste.pdf'
