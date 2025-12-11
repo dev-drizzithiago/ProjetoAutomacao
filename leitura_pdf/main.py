52a83cd72f5b0ca650800e5c86dcd2a2902e4d8e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-12-11 15:22:18 -0300

    Update main.py

--- leitura_pdf/main.py
+++ leitura_pdf/main.py
@@ -277,7 +277,7 @@ class LeituraPdf:
         # print(totais_estabelecimentos)
 
         bloco_27_total_debito_declarado = re.compile(
-            r'\s*Total\ do\ Débito\ Declarado\s*(\s*exigível\ + suspenso\s*)'
+            r'\s*Total\ do\ Débito\ Declarado\s*.*?(?=2\.8\))'
 
         )
         total_debito_exigibilidade = bloco_27_total_debito_declarado.search(bloco_27)
