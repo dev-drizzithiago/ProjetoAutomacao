5dea8ee638d04b193d3d0da808aedecf0988c78f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-08 15:57:39 -0300

    Update main.py

--- anydesk/main.py
+++ anydesk/main.py
@@ -17,8 +17,8 @@ class GeranciadorDePacotes:
         print(response_powershell.stdout)
 
     def desinstalar_pocotes(self):
-        pacote = ' anydesk'
-        comando_shell = f"winget uninstall --id {pacote} --si"
+        pacote = 'AnyDeskSoftwareGmbH.AnyDesk'
+        comando_shell = f"winget uninstall --id {pacote} --silent"
 
         response_powershell = subprocess.run(
             ['powershell', '-Command', comando_shell],
@@ -27,7 +27,11 @@ class GeranciadorDePacotes:
 
         print(response_powershell.stdout)
 
+    def get_Processos(self):
+        comando_shell = "Get-Process -Name 'AnyDesk*' -ErrorAction SilentlyContinue | Stop-Process -Force"
+
+
 if __name__ == '__main__':
     obj_pacote = GeranciadorDePacotes()
 
-    obj_pacote.procurar_pacotes()
+    obj_pacote.desinstalar_pocotes()
