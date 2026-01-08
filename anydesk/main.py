99d406b5161d52955dd3f9e7a323eb5d70e75b07
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-08 15:10:15 -0300

    Update main.py

--- anydesk/main.py
+++ anydesk/main.py
@@ -7,15 +7,25 @@ class GeranciadorDePacotes:
 
     def procurar_pacotes(self):
         pacote = ' anydesk'
-        comando_powershell = f"winget search {pacote}"
+        comando_shell = f"winget search {pacote}"
 
         response_powershell = subprocess.run(
-            ['powershell', '-Command', comando_powershell],
+            ['powershell', '-Command', comando_shell],
             text=True, capture_output=True
         )
 
         print(response_powershell.stdout)
 
+    def desinstalar_pocotes(self):
+        pacote = ' anydesk'
+        comando_shell = f"winget uninstall --id {pacote} --si"
+
+        response_powershell = subprocess.run(
+            ['powershell', '-Command', comando_shell],
+            text=True, capture_output=True
+        )
+
+        print(response_powershell.stdout)
 
 if __name__ == '__main__':
     obj_pacote = GeranciadorDePacotes()
