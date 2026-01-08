408098fca9ad0691af994ded012336937cca9e95
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-08 13:59:36 -0300

    Update main.py

---
+++ anydesk/main.py
@@ -0,0 +1,13 @@
+
+
+class GeranciadorDePacotes:
+    def __init__(self):
+        pass
+
+
+    def procurar_pacotes(self):
+        pass
+
+
+if __name__ == '__main__':
+    obj_pacote = GeranciadorDePacotes()
