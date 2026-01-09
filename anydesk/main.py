9cee6db167592ec5a75ceeeb78e76072a3065d15
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-09 15:28:25 -0300

    Update main.py

--- anydesk/main.py
+++ anydesk/main.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+from platform import system
 from time import sleep
 
 import ctypes
@@ -37,71 +38,50 @@ class GeranciadorDePacotes:
     def __init__(self):
         pass
 
-    def procurar_pacote_anydesk(self):
-
-        print('Procurando por um pacote AnyDesk instalado')
-
-        pacote = 'anydesk'
-        comando_shell = f"winget search {pacote}"
-
-        response_powershell = subprocess.run(
-            ['powershell', '-Command', comando_shell],
-            text=True, capture_output=True
-        )
-        resultado_busca = response_powershell.stdout
-
-        print(resultado_busca)
-
-    def instalar_pacote_anydesk(self):
-        comando_shell = (
-            f'Start-Process "{path_file_name}" --silent'
-        )
-
-        response_powershell = subprocess.run(
-            ['powershell', '-Command', comando_shell],
-            text=True, capture_output=True
-        )
-
-        print(response_powershell.stdout)
-
-    def desinstalar_pocotes(self):
-        pacote = 'AnyDesk.AnyDesk'
-        comando_shell = f"winget uninstall --id {pacote} --silent"
-
-        response_powershell = subprocess.run(
-            ['powershell', '-Command', comando_shell],
-            text=True, capture_output=True
-        )
-
-        print(response_powershell.stdout)
-
-    def procurando_processo_anydsk(self):
-        comando_shell = rf'Get-Process | Where-Object {{ $_.ProcessName -like "*AnyDesk*" }}; exit 0'
-
-        response_powershell = subprocess.run(
-            ['powershell', '-Command', comando_shell],
-            text=True, capture_output=True
-        )
-        resultado_busca = response_powershell.stdout
-        print(resultado_busca)
-
-    def abrir_processo(self):
-        print('Abrindo pacote')
-        caminho_app = r"C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
-
-        # comando_shell = fr'Start-Service AnyDesk'
-        # comando_shell = fr'Start-Process "{caminho_app}" -ArgumentList "--tray"'
-        comando_shell = fr'Start-Process "{caminho_app}" -WindowStyle Minimized'
-
-        response_powershell = subprocess.run(
-            ['powershell', '-Command', comando_shell],
-            text=True, capture_output=True
-        )
-
-        print(response_powershell.stdout)
+    # def procurar_pacote_anydesk(self):
+    #
+    #     print('Procurando por um pacote AnyDesk instalado')
+    #
+    #     pacote = 'anydesk'
+    #     comando_shell = f"winget search {pacote}"
+    #
+    #     response_powershell = subprocess.run(
+    #         ['powershell', '-Command', comando_shell],
+    #         text=True, capture_output=True
+    #     )
+    #     resultado_busca = response_powershell.stdout
+    #
+    #     print(resultado_busca)
+    #
+    # def instalar_pacote_anydesk(self):
+    #     comando_shell = (
+    #         f'Start-Process "{path_file_name}" --silent'
+    #     )
+    #
+    #     response_powershell = subprocess.run(
+    #         ['powershell', '-Command', comando_shell],
+    #         text=True, capture_output=True
+    #     )
+    #
+    #     print(response_powershell.stdout)
+    #
+    # def desinstalar_pocotes(self):
+    #     pacote = 'AnyDesk.AnyDesk'
+    #     comando_shell = f"winget uninstall --id {pacote} --silent"
+    #
+    #     response_powershell = subprocess.run(
+    #         ['powershell', '-Command', comando_shell],
+    #         text=True, capture_output=True
+    #     )
+    #
+    #     print(response_powershell.stdout)
 
     def remover_processo(self):
-        print('Fechando Processo do Anydesk')
+        os.system('cls')
+        print()
+        print('Fechando Processo do Anydesk, aguarde...')
+        print('---' * 30)
+        print()
         comando_shell = "Get-Process -Name 'AnyDesk*' -ErrorAction SilentlyContinue | Stop-Process -Force"
         # comando_shell = "Get-Process -Name 'AnyDesk*' -ErrorAction SilentlyContinue"
 
@@ -109,24 +89,53 @@ class GeranciadorDePacotes:
             ['powershell', '-Command', comando_shell],
             text=True, capture_output=True
         )
-        print(response_powershell.stdout)
+        if response_powershell.returncode == 0:
+            print('Processo finalizado, aguardando próximo processo...')
 
     def removendo_config_anydesk(self):
+        os.system('cls')
+        print()
         print('Removendo Configurações do Anydesk')
+        print('---' * 30)
+        print()
         caminho_confi_anydesk = r"C:\ProgramData\AnyDesk"
 
         if os.path.exists(caminho_confi_anydesk):
             shutil.rmtree(caminho_confi_anydesk)
 
+        print('Configuração removida, aguardando próximo processo...')
+        sleep(3)
+
+    def abrir_processo(self):
+        os.system('cls')
+        print()
+        print('Abrindo o AnyDesk, aguarde...')
+        print('---' * 30)
+        print()
+        caminho_app = r"C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
+        comando_shell = fr'Start-Process "{caminho_app}" -WindowStyle Minimized'
+
+        response_powershell = subprocess.run(
+            ['powershell', '-Command', comando_shell],
+            text=True, capture_output=True
+        )
 
 if __name__ == '__main__':
 
     if verificar_elevacao():
         obj_pacote = GeranciadorDePacotes()
+        # obj_pacote.remover_processo()
+        # sleep(5)
+        # obj_pacote.removendo_config_anydesk()
+        # sleep(5)
+        # obj_pacote.abrir_processo()
 
-        obj_pacote.procurando_processo_anydsk()
+        print()
         input('Aperte ENTER para continuar...')
-        # # obj_pacote.remover_processo()
-        #
-        # print('Finalizando o processo.')
-        sleep(5)
+        os.system('cls')
+
+        for contagem in range(5, 1, -1):
+            print('Finalizando em ', end=' ')
+            print(contagem)
+            sleep(1)
+            os.system('cls')
