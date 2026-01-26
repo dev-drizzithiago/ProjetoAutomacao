3181f38949016de46c3473295d91664a8e783361
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-26 16:41:00 -0300

    Update main.py

--- softwares_instalados/main.py
+++ softwares_instalados/main.py
@@ -1,3 +1,4 @@
+import re
 from subprocess import PIPE, run
 
 
@@ -30,9 +31,25 @@ class RelatorioSoftwareInstalados:
             text=True,  stdout=PIPE
         )
         for item in response_scan.stdout.splitlines():
-            self.lista_itens.append(item)
+            formatacao_item = re.sub(r"\s+", " ", item)
 
-        print(self.lista_itens)
+
+            self.lista_itens.append(formatacao_item)
+
+        for row in self.lista_itens:
+            linha = (row or '').strip()
+
+            if not linha:
+                continue
+
+            if linha.startswith('DisplayName') or set(linha) <= {'-', ' '}:
+                continue
+
+            regex_result = re.search(r'(\d+(?:\.\d+)+)\s*$', linha)
+
+            print(regex_result)
+            print()
+            print(linha)
 
 if __name__ == '__main__':
     obj_scan_software = RelatorioSoftwareInstalados()
