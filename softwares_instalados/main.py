ec452474edb66afda5570da44bf7b81788b8ef43
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-26 14:50:29 -0300

    Update main.py

--- softwares_instalados/main.py
+++ softwares_instalados/main.py
@@ -15,12 +15,13 @@ class RelatorioSoftwareInstalados:
     def __init__(self):
         pass
 
-
     def scan_software(self):
         response_scan = subprocess.run(
             ["powershell -Command", self.CAMANDO_SCAN_SOFTWARE_MICROSOFT],
             text=True, capture_output=True
         )
+        print(response_scan.stdout)
 
 if __name__ == '__main__':
     obj_scan_software = RelatorioSoftwareInstalados()
+    obj_scan_software.scan_software()
