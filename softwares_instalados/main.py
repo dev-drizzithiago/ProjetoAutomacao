ce7481b07ad7e8c2f4b00e9f913ffd1c293b1f48
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-26 14:48:00 -0300

    Create main.py

---
+++ softwares_instalados/main.py
@@ -0,0 +1,26 @@
+import subprocess
+
+
+
+class RelatorioSoftwareInstalados:
+
+    CAMANDO_SCAN_SOFTWARE_MICROSOFT = (
+        r"Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* , "
+        r"HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | "
+        r"Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | "
+        r"Where-Object { $_.DisplayName } | "
+        r"Sort-Object DisplayName "
+    )
+
+    def __init__(self):
+        pass
+
+
+    def scan_software(self):
+        response_scan = subprocess.run(
+            ["powershell -Command", self.CAMANDO_SCAN_SOFTWARE_MICROSOFT],
+            text=True, capture_output=True
+        )
+
+if __name__ == '__main__':
+    obj_scan_software = RelatorioSoftwareInstalados()
