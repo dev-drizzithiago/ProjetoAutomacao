6590ffc9a66fc1a3db5c5905e1bd2d0ef48b1182
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-28 15:34:31 -0300

    Update __init__.py

--- softwares_instalados/app_planilha_excel/__init__.py
+++ softwares_instalados/app_planilha_excel/__init__.py
@@ -1,11 +1,12 @@
-
+import pandas as pd
 
 
 class CreaterPlanilha:
-    def __init__(self):
-        pass
 
-    def criar_planilha_dados_app(self, dados_entrada):
-        for item in dados_entrada:
+    def dados_to_pandas(self, dados_entrada):
+        data_frame_app = pd.DataFrame(dados_entrada)
+        print(data_frame_app)
+
+    def criar_planilha_dados_app(self, dados_pandas):
+        for item in dados_pandas:
             print(f"{item['DisplayName']} => {item['DisplayVersion']}")
-        pass
