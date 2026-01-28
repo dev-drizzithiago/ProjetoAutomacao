20f3cbb89474cf2a7456f842e911cd009442e269
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-28 15:58:47 -0300

    Update __init__.py

--- softwares_instalados/app_planilha_excel/__init__.py
+++ softwares_instalados/app_planilha_excel/__init__.py
@@ -2,10 +2,23 @@ import pandas as pd
 
 
 class CreaterPlanilha:
-    CAMINHO_ABS_PLANILHA = 'PLANILHA-TESTE.xlsx'
-    def dados_to_pandas(self, dados_entrada):
-        data_frame_app = pd.DataFrame(dados_entrada)
 
-    def criar_planilha_dados_app(self, dados_pandas):
+    def __init__(self):
+        self.DATA_FRAME_APP = None
+        self.CAMINHO_ABS_PLANILHA = None
+
+
+    def dados_to_pandas(self, dados_entrada):
+        self.CAMINHO_ABS_PLANILHA = f"{
+        dados_entrada['DisplayName'] - str(dados_entrada['DisplayVersion']).replace('.', '-')
+        }"
+        self.DATA_FRAME_APP = pd.DataFrame(dados_entrada)
+
+    def criar_planilha_dados_app(self):
+
+        print('Criando a planilha!')
+
+        print()
         with pd.ExcelWriter(self.CAMINHO_ABS_PLANILHA, engine='xlsxwriter') as writer:
             sheet = 'Relatório APPs'
+            self.DATA_FRAME_APP.to_excel(writer, sheet_name=sheet, index=False)
