43d4a15a225b87af84d94d65f37314058a5e6cfa
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-01-30 15:05:21 -0300

    Update novo_note.py

--- utilitarios_segeti/novo_note.py
+++ utilitarios_segeti/novo_note.py
@@ -1,11 +1,75 @@
+from subprocess import run, PIPE
+from time import sleep
+
+USUARIO_LOCAL = ''
+PASSWORD_USER_LOCAL = ''
+PASTA_COMPARTILHADA = 'Digitalização'
+
 
 class ConfigNovoNote:
+
+
     def __init__(self):
         pass
 
+    def criar_usuario_adm_local(self):
+        script_criar_user = (
+            rf'Get-Credential -Message "Defina a senha para .\ti" -UserName ".\ti" | '
+            rf'Export-Clixml -Path "C:\Creds\cred_local_ti.clixml"'
+        )
+
+        run([
+            "powershell",
+            "-NoProfile",
+            "-ExecutionPolicy",
+            "Bypass",
+            "-Command",
+            script_criar_user
+        ], text=True,  # retorna stdout como str (não bytes)  faz o stdout vir já como string.
+            stdout=PIPE  # captura a saída padrão para uso no Python  captura a saída do comando dentro do Python.
+        )
+
     def conf_pasta_scan(self):
-        pass
+        SCRIPT_CRIAR_PASTA = rf"""
+            New-Item -ItemType Directory -Path {PASTA_COMPARTILHADA} -Force | Out-Null
+        """
+        print(f'Criando pasta: "{PASTA_COMPARTILHADA}"...')
+        run([
+            "powershell",
+            "-NoProfile",
+            "-ExecutionPolicy",
+            "Bypass",
+            "-Command",
+            SCRIPT_CRIAR_PASTA
+        ],  text=True,   # retorna stdout como str (não bytes)  faz o stdout vir já como string.
+            stdout=PIPE  # captura a saída padrão para uso no Python  captura a saída do comando dentro do Python.
+        )
+
+        SCRIPT_COMPARTILHAMENTO = rf"""
+            New-SmbShare -Name {PASTA_COMPARTILHADA} 
+            -Path $Pasta -Description "Compartilhamento restrito para .\ti" \
+            -CachingMode None | Out-Null
+        """
+
+        print(f'Configurando compartilhamento da pasta: "{PASTA_COMPARTILHADA}"')
+        sleep(5)
+
+        run([
+            "powershell",
+            "-NoProfile",
+            "-ExecutionPolicy",
+            "Bypass",
+            "-Command",
+            SCRIPT_COMPARTILHAMENTO
+        ], text=True,  # retorna stdout como str (não bytes)  faz o stdout vir já como string.
+            stdout=PIPE  # captura a saída padrão para uso no Python  captura a saída do comando dentro do Python.
+        )
+
 
     def conf_dominio(self):
         pass
-    
+
+
+if __name__ == '__main__':
+    init_obj_config = ConfigNovoNote()
+    init_obj_config.conf_pasta_scan()
