ffc50e82f0d3d7f844944103f79f94878da0dfeb
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-02 15:57:38 -0300

    Update __init__.py

--- softwares_instalados/spinner/__init__.py
+++ softwares_instalados/spinner/__init__.py
@@ -1,9 +1,11 @@
 import itertools
 from time import sleep
 from subprocess import run
+from threading import Event, Thread
 
 
-def _run_processo_powershell(self, comando_shell):
+def _run_processo_powershell(comando_shell):
+    sleep(10)
     resultado_processo = run(
         ["powershell", "-NoProfile", "-ExecutionPolicy", "Bypass", "-Command", comando_shell],
         shell=True,
@@ -14,7 +16,7 @@ def _run_processo_powershell(self, comando_shell):
     return resultado_processo
 
 
-def _spinner(self, stop_event, prefix='Processando... '):
+def _spinner(stop_event, prefix='Processando... '):
     ciclo = itertools.cycle(['|', '/', '-', '\\'])
 
     while not stop_event.is_set():
@@ -26,12 +28,12 @@ def _spinner(self, stop_event, prefix='Processando... '):
     print('\n' + ' ' * 60 + '\r', end='', flush=True)
 
 
-def _run_spinner(self, comando_str, texto_spinner):
+def _run_spinner(comando_str, texto_spinner):
     stop_event = Event()
-    _thread = Thread(target=self._spinner, args=(stop_event, texto_spinner), daemon=True)
+    _thread = Thread(target=_spinner, args=(stop_event, texto_spinner), daemon=True)
     _thread.start()
     try:
-        result = self._run_processo_powershell(comando_str)
+        result = _run_processo_powershell(comando_str)
         return result
     finally:
         stop_event.set()
