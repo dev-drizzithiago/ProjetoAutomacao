3c21ccd4fd2656979cd09197f9aad0e324549e6f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-02 15:26:14 -0300

    Update __init__.py

--- softwares_instalados/app_planilha_excel/__init__.py
+++ softwares_instalados/app_planilha_excel/__init__.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from time import sleep
 
 class CreaterPlanilha:
 
@@ -29,6 +30,7 @@ class CreaterPlanilha:
         self.DATA_FRAME_APP = pd.DataFrame(dados_entrada)
 
     def criar_planilha_dados_app(self):
+        os.system('cls')
         print()
         print('---' * 10)
         print('Criando a Planilha...!')
@@ -43,6 +45,7 @@ class CreaterPlanilha:
             print(self.local_save_planilha)
             self.local_save_planilha = os.path.join(self.CAMINHO_ABS_SERVIDOR, self.NOME_PLANILHA)
         except:
+            os.system('cls')
             print()
             print('---' * 10)
             print('Servidor não respondeu, arquivo sendo salvo no computador local...')
@@ -51,35 +54,50 @@ class CreaterPlanilha:
 
         # Abre um ExcelWriter apontando para o caminho absoluto
         # engine='xlsxwriter': usa o motor xlsxwriter (excelente para formatação rica).
-        with pd.ExcelWriter(self.local_save_planilha, engine='xlsxwriter') as writer:
+        try:
+            with pd.ExcelWriter(self.local_save_planilha, engine='xlsxwriter') as writer:
 
-            # sheet: nome da planilha na aba.
-            sheet = 'Relatório APPs'
+                # sheet: nome da planilha na aba.
+                sheet = 'Relatório APPs'
 
-            wb = writer.book
+                wb = writer.book
 
-            # Escreve os dados do DataFrame no arquivo Excel, sem a coluna de índice.
-            self.DATA_FRAME_APP.to_excel(writer, sheet_name=sheet, index=False)
+                # Escreve os dados do DataFrame no arquivo Excel, sem a coluna de índice.
+                self.DATA_FRAME_APP.to_excel(writer, sheet_name=sheet, index=False)
 
-            work_sheet = writer.sheets[sheet]
-            bold = wb.add_format({'bold': True})
+                work_sheet = writer.sheets[sheet]
+                bold = wb.add_format({'bold': True})
 
-            work_sheet.set_column("A:A", 80, bold)
-            work_sheet.set_column("B:B", 25)
+                work_sheet.set_column("A:A", 80, bold)
+                work_sheet.set_column("B:B", 25)
 
-            rows, cols = self.DATA_FRAME_APP.shape
+                rows, cols = self.DATA_FRAME_APP.shape
 
-            work_sheet.add_table(0, 0, rows, cols - 1, {
-                "name": "TabelaSoftware",
+                work_sheet.add_table(0, 0, rows, cols - 1, {
+                    "name": "TabelaSoftware",
 
-                # Define um estilo de tabela (TableStyleMedium9) — dá zebra e filtros nativos.
-                "style": "TableStyleMedium9",
+                    # Define um estilo de tabela (TableStyleMedium9) — dá zebra e filtros nativos.
+                    "style": "TableStyleMedium9",
 
-                # Define o texto do cabeçalho de cada coluna a partir de df.columns.
-                "columns": [{"header": c} for c in self.DATA_FRAME_APP.columns]
-            })
+                    # Define o texto do cabeçalho de cada coluna a partir de df.columns.
+                    "columns": [{"header": c} for c in self.DATA_FRAME_APP.columns]
+                })
 
-            work_sheet.freeze_panes(1, 0)
-            work_sheet.freeze_panes(2, 0)
+                work_sheet.freeze_panes(1, 0)
+                work_sheet.freeze_panes(2, 0)
+            os.system('cls')
+            print()
+            print('---' * 10)
+            print('Planilha criada com sucesso!')
+            sleep(5)
+            return self.NOME_PLANILHA
+        except Exception as error:
+            os.system('cls')
 
-        return self.NOME_PLANILHA
+            print()
+            print('---' * 10)
+            print('Erro ao criar a planilha: ', error)
+
+            print()
+            print('---' * 10)
+            input('Aperta ENTER para finalizar!')
