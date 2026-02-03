6e666dc6fdcc9e5505fcb994fd7222474c5494ba
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-03 16:28:44 -0300

    Update __init__.py

--- softwares_instalados/coletando_info_harware/__init__.py
+++ softwares_instalados/coletando_info_harware/__init__.py
@@ -21,36 +21,43 @@ class InfoHardWareScan:
         result_busca_processador = self.conn_hardware.Win32_Processor()
         result_busca_memoria = self.conn_hardware.Win32_PhysicalMemory()
         result_busca_placa_mae = self.conn_hardware.Win32_BaseBoard()
-        result_busca_placa_disk = self.conn_hardware.Win32_LogicalDisk()
+        result_busca_disk = self.conn_hardware.Win32_LogicalDisk()
 
-        # for listagem in result_busca_processador:
-        #     self.dict_info_hardware['Processador']= listagem.Name
-        #     self.dict_info_hardware['Números Cores']= listagem.NumberOfCores
-        #     self.dict_info_hardware['Número de Threads']= listagem.NumberOfLogicalProcessors
-        #
-        # for listagem in result_busca_memoria:
-        #     self.dict_info_hardware['Memoria'] = listagem.Name
-        #     self.dict_info_hardware['Capacidade'] = str(int (listagem.Capacity) / 10243).split('.')[0]
-        #     self.dict_info_hardware['Clock Speed'] = listagem.ConfiguredClockSpeed
-        #     self.dict_info_hardware['Velocidade'] = listagem.Speed
-        #     self.dict_info_hardware['Parte Number'] = listagem.PartNumber
-        #     self.dict_info_hardware['Serial Number'] = listagem.SerialNumber
+        for listagem in result_busca_processador:
+            self.dict_info_hardware['Busca'] = 'Processadores'
+            self.dict_info_hardware['Modelo'] = listagem.Name
+            self.dict_info_hardware['Números Cores'] = listagem.NumberOfCores
+            self.dict_info_hardware['Número de Threads'] = listagem.NumberOfLogicalProcessors
 
-        # for listagem in result_busca_placa_mae:
-        #     self.dict_info_hardware['Placa Mae'] = listagem.Name
-        #     self.dict_info_hardware['Fabricante'] = listagem.Manufacturer
-        #     self.dict_info_hardware['Serial Number'] = listagem.SerialNumber
-        #     self.dict_info_hardware['Numero Produto'] = listagem.Product
-        #     self.dict_info_hardware['Versao'] = listagem.Version
 
-        for listagem in result_busca_placa_disk:
+        for listagem in result_busca_memoria:
+            self.dict_info_hardware['Busca'] = 'RAM'
+            self.dict_info_hardware['Modelo'] = listagem.Name
+            self.dict_info_hardware['Capacidade'] = str(int (listagem.Capacity) / 10243).split('.')[0]
+            self.dict_info_hardware['Clock Speed'] = listagem.ConfiguredClockSpeed
+            self.dict_info_hardware['Velocidade'] = listagem.Speed
+            self.dict_info_hardware['Parte Number'] = listagem.PartNumber
+            self.dict_info_hardware['Serial Number'] = listagem.SerialNumber
+
+        for listagem in result_busca_placa_mae:
+            self.dict_info_hardware['Busca'] = 'MainBoard'
+            self.dict_info_hardware['Placa Mae'] = listagem.Name
+            self.dict_info_hardware['Fabricante'] = listagem.Manufacturer
+            self.dict_info_hardware['Serial Number'] = listagem.SerialNumber
+            self.dict_info_hardware['Numero Produto'] = listagem.Product
+            self.dict_info_hardware['Versao'] = listagem.Version
+
+        for listagem in result_busca_disk:
             if listagem.DeviceID == "C:":
+                porcetangem_espaco_livre = 100*float(listagem.FreeSpace) / float(listagem.Size)
                 self.dict_info_hardware['Disco Local'] = listagem.Description
-                self.dict_info_hardware['Capacidade'] = str(int(listagem.Size) / 10243).split('.')[]
-                self.dict_info_hardware['Espaço Livre'] = listagem.FreeSpace
+                self.dict_info_hardware['Capacidade'] =  str(int(listagem.Size) / 10243).split('.')[0]
+                self.dict_info_hardware['Espaço Livre'] = f"{porcetangem_espaco_livre:.2f}"
                 self.dict_info_hardware['Numero de Serie'] = listagem.VolumeSerialNumber
 
-        print(self.dict_info_hardware)
+
+        for k, v in self.dict_info_hardware.items():
+            print(f'{k} - {v}')
 
     def _spinner(self, stop_event, prefix='Processando... '):
         ciclo = itertools.cycle(['|', '/', '-', '\\'])
