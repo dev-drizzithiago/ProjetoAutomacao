de7efd1ece81be9372e3eb635bc90880f87e739f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-03 11:34:55 -0300

    Update __init__.py

--- softwares_instalados/coletando_info_harware/__init__.py
+++ softwares_instalados/coletando_info_harware/__init__.py
@@ -1,4 +1,7 @@
 import wmi
+import itertools
+from time import sleep
+from threading import Event, Thread
 
 
 class InfoHardWareScan:
@@ -10,8 +13,31 @@ class InfoHardWareScan:
     def scan_hardware(self):
         return  self.conn_hardware.classes
 
+    def _spinner(self, stop_event, prefix='Processando... '):
+        ciclo = itertools.cycle(['|', '/', '-', '\\'])
+
+        while not stop_event.is_set():
+            # \r volta o cursor para início da linha
+            print(f"\r{prefix} {next(ciclo)}", end='', flush=True)
+            sleep(0.1)
+
+        # limpa linha ao finalizar
+        print('\n' + ' ' * 60 + '\r', end='', flush=True)
+
+    def _run_spinner(self, texto_spinner):
+        stop_event = Event()
+        _thread = Thread(target=self._spinner, args=(stop_event, texto_spinner), daemon=True)
+        _thread.start()
+        try:
+            result = self.scan_hardware()
+            return result
+        finally:
+            stop_event.set()
+            _thread.join()
+
 if __name__ == '__main__':
     inicit_obj_scan = InfoHardWareScan()
-    result_scan = inicit_obj_scan.scan_hardware()
+    result_scan = inicit_obj_scan._run_spinner('Buscando informações sobre o hardware')
 
     print(result_scan)
+
