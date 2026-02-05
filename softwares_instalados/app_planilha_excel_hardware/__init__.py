a6c111f46bf67145aa901449ae1156f858e10ca4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-05 11:43:00 -0300

    Update __init__.py

--- softwares_instalados/app_planilha_excel_hardware/__init__.py
+++ softwares_instalados/app_planilha_excel_hardware/__init__.py
@@ -31,8 +31,6 @@ class CreaterPlanilhaHardware:
             pass
 
     def dados_to_pandas(self):
-        for item in self.dados_de_entrada:
-            print(item)
 
         grupo_componentes = {
             'MainBoard': [],
@@ -51,21 +49,20 @@ class CreaterPlanilhaHardware:
                 if componente in grupo_componentes:
                     grupo_componentes[componente].append(payload)
 
-        # print(grupo_componentes)
 
         if grupo_componentes['MainBoard']:
             self.df_mb = pd.DataFrame(grupo_componentes['MainBoard'])
         else:
             self.df_mb = pd.DataFrame(columns=['Placa Mae','Fabricante','Serial Number','Numero Produto','Versao'])
             # self.dataFrama_hardware = pd.DataFrame(componente)
-        # print(self.df_mb)
+        print(self.df_mb)
 
         # Verificar se possui mais de 1 processador. Caso tenha mais é criado linhas adicionais;
         if grupo_componentes['Processador']:
             self.df_main = pd.DataFrame(grupo_componentes['Processador'])
         else:
             self.df_main = pd.DataFrame(columns=['Modelo', 'Números Cores', 'Número de Threads'])
-        # print(self.df_main)
+        print(self.df_main)
 
         # Verificar se possui mais de 1 modulo de RAM. Caso tenha mais é criado linhas adicionais;
         if grupo_componentes['Memoria']:
@@ -79,7 +76,7 @@ class CreaterPlanilhaHardware:
                          'Parte Number',
                          'Serial Number']
             )
-        # print(self.df_ram)
+        print(self.df_ram)
 
         if grupo_componentes['HDD_SSD']:
             self.ssd_hdd = pd.DataFrame(grupo_componentes['HDD_SSD'])
