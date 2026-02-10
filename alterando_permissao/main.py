46e9eb9d4b09450b423c77be8859184ad3580fb2
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-10 14:01:15 -0300

    Update main.py

--- alterando_permissao/main.py
+++ alterando_permissao/main.py
@@ -2,12 +2,15 @@ from conectando_exechange_online import ConexaoExchangeOnline
 
 class AlterarPermissaoReunioes:
     def __init__(self, *credenciais):
-        pass
+        self.init_conectar_exchange = ConexaoExchangeOnline()
 
     def chamando_obj_conexao(self):
-        init_conectar_exchange = ConexaoExchangeOnline()
-        init_conectar_exchange.install_modulo_exchange()
+        self.init_conectar_exchange = ConexaoExchangeOnline()
+        self.init_conectar_exchange.install_modulo_exchange()
+        self.init_conectar_exchange.importando_modulo()
+        self.init_conectar_exchange.conectando_exchange()
 
 if __name__ == '__main__':
     init_obj_calendar = AlterarPermissaoReunioes()
     init_obj_calendar.chamando_obj_conexao()
+    
