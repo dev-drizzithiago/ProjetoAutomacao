85e3ecbe1fc1c048ddc19cf04473c38dbfde1c04
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-10 14:47:03 -0300

    Update main.py

--- alterando_permissao/main.py
+++ alterando_permissao/main.py
@@ -4,12 +4,23 @@ class AlterarPermissaoReunioes:
     def __init__(self, *credenciais):
         self.init_conectar_exchange = ConexaoExchangeOnline()
 
-    def chamando_obj_conexao(self):
+    def chamando_obj_conexao(self, cmd, text):
         self.init_conectar_exchange = ConexaoExchangeOnline()
-        self.init_conectar_exchange.install_modulo_exchange()
-        self.init_conectar_exchange.importando_modulo()
-        self.init_conectar_exchange.conectando_exchange('thiago.pinheiro@segeticonsultoria.com')
+        self.init_conectar_exchange.processando_modulo_exchange(cmd, text)
+
 
 if __name__ == '__main__':
     init_obj_calendar = AlterarPermissaoReunioes()
-    init_obj_calendar.chamando_obj_conexao()
+    init_obj_calendar.chamando_obj_conexao(
+        "Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force",
+        'Instalando o modúlo... '
+    )
+    init_obj_calendar.chamando_obj_conexao(
+        "Import-Module ExchangeOnlineManagement",
+        'Importando Modúlo... '
+    )
+    init_obj_calendar.chamando_obj_conexao(
+        "Connect-ExchangeOnline -UserPrincipalName thiago.pinheiro@segeticonsultoria.com",
+        'Conectando ao modulo... '
+    )
+
