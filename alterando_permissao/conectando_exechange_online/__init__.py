9a298f49b010de7eb6acccd008b16a42d9d0c283
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-10 15:56:20 -0300

    Update __init__.py

--- alterando_permissao/conectando_exechange_online/__init__.py
+++ alterando_permissao/conectando_exechange_online/__init__.py
@@ -8,7 +8,9 @@ class ProcessoRun:
     def _run_processo_powershell(self, comando_shell):
         resultado_processo = run(
             ["powershell", "-Command", comando_shell],
-            text=True, capture_output=True
+            shell=True,
+            check=True,
+            capture_output=True
         )
 
         return resultado_processo
