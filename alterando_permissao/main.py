b75e2b68011e336bac76eae2697cf4dc7cd32cf6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-10 15:52:18 -0300

    Update main.py

--- alterando_permissao/main.py
+++ alterando_permissao/main.py
@@ -1,26 +1,22 @@
-from conectando_exechange_online import ConexaoExchangeOnline
+from conectando_exechange_online import ProcessoRun
 
 class AlterarPermissaoReunioes:
-    def __init__(self, *credenciais):
-        self.init_conectar_exchange = ConexaoExchangeOnline()
+    def __init__(self):
+        self.cmd = """
+                Connect-ExchangeOnline `
+                -AppId 00000002-0000-0ff1-ce00-000000000000 `
+                -CertificateThumbprint 7278d039-3161-4e91-8d8f-4038e71aa776 `
+                -Organization segeticonsultoria.onmicrosoft.com
+                """
+        self.init_conectar_exchange = ProcessoRun()
 
-    def chamando_obj_conexao(self, cmd, text):
-        self.init_conectar_exchange = ConexaoExchangeOnline()
-        self.init_conectar_exchange.processando_modulo_exchange(cmd, text)
+    def chamando_obj_conexao(self):
+        self.init_conectar_exchange = ProcessoRun()
+        self.init_conectar_exchange.run_spinner(self.cmd, 'Conectando... ')
 
 
 if __name__ == '__main__':
     init_obj_calendar = AlterarPermissaoReunioes()
-    init_obj_calendar.chamando_obj_conexao(
-        "Install-Module ExchangeOnlineManagement -Scope CurrentUser -Force",
-        'Instalando o modúlo... '
-    )
-    init_obj_calendar.chamando_obj_conexao(
-        "Import-Module ExchangeOnlineManagement",
-        'Importando Modúlo... '
-    )
-    init_obj_calendar.chamando_obj_conexao(
-        "Connect-ExchangeOnline -UserPrincipalName thiago.pinheiro@segeticonsultoria.com",
-        'Conectando ao modulo... '
-    )
+    init_obj_calendar.chamando_obj_conexao()
+
 
