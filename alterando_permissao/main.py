fc907381478e2db233defead130fea4dde37c625
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2026-02-26 16:26:23 -0300

    Update main.py

--- alterando_permissao/main.py
+++ alterando_permissao/main.py
@@ -95,35 +95,35 @@ class AlterarPermissaoReunioes:
             # Funcionando 
             
             # 2) Criar o mailbox compartilhado (se não existir);
-            $shared = Get-Mailbox -Identity $sharedSmtp -ErrorAction SilentlyContinue           
+            # $shared = Get-Mailbox -Identity $sharedSmtp -ErrorAction SilentlyContinue           
+            # 
+            # if (-not $shared) {{
+            #     Write-Host "Criando mailbox compartilhado $sharedSmtp" -ForegroundColor Cyan 
+            #     New-Mailbox -Shared -Name "{os.getenv('NOME_GRUPO')}" -PrimarySmtpAddress $sharedSmtp ` 
+            #         -ErrorAction Stop
+            # }} else {{
+            #     Write-Host "! já existe o Mailbox compartilhado com o endereço: $sharedSmtp" -ForegroundColor Yellow 
+            # }} 
+            # 
+            # # 2° etapa; concedendo permissões e testes
+            # # 3) Conceder permissões (FullAccess + SendAs) aos membros listados no CSV; 
+            # 
+            # if (-not $sharedSmtp -or -not $usuario) {{
+            #     throw "Defina ORGANIZADOR_GRUPO (shared SMTP) e USUARIO_TESTE (UPN)." 
+            # }} 
+
+            # Write-Host ">> Concedendo FullAccess a $usuario no shared $sharedSmtp ..." -ForegroundColor Cyan 
+            # try {{
+            #     Add-MailboxPermission -Identity $sharedSmtp `
+            #     -User $usuario -AccessRights FullAccess -AutoMapping:$true -ErrorAction Stop 
+            #     Write-Host "✓ FullAccess concedido" -ForegroundColor Green 
+            # }} catch {{
+            #     if ($_.Exception.Message -match 'already on the permission entry list') {{
+            #         Write-Host "! FullAccess já existia" -ForegroundColor Yellow
+            #     }} else {{ throw }}
+            # }} 
             
-            if (-not $shared) {{
-                Write-Host "Criando mailbox compartilhado $sharedSmtp" -ForegroundColor Cyan 
-                New-Mailbox -Shared -Name "{os.getenv('NOME_GRUPO')}" -PrimarySmtpAddress $sharedSmtp ` 
-                    -ErrorAction Stop
-            }} else {{
-                Write-Host "! já existe o Mailbox compartilhado com o endereço: $sharedSmtp" -ForegroundColor Yellow 
-            }} 
-
-            # 2° etapa; concedendo permissões e testes
-            # 3) Conceder permissões (FullAccess + SendAs) aos membros listados no CSV; 
-
-            if (-not $sharedSmtp -or -not $usuario) {{
-                throw "Defina ORGANIZADOR_GRUPO (shared SMTP) e USUARIO_TESTE (UPN)." 
-            }} 
-
-            Write-Host ">> Concedendo FullAccess a $usuario no shared $sharedSmtp ..." -ForegroundColor Cyan 
-            try {{
-                Add-MailboxPermission -Identity $sharedSmtp `
-                -User $usuario -AccessRights FullAccess -AutoMapping:$true -ErrorAction Stop 
-                Write-Host "✓ FullAccess concedido" -ForegroundColor Green 
-            }} catch {{
-                if ($_.Exception.Message -match 'already on the permission entry list') {{
-                    Write-Host "! FullAccess já existia" -ForegroundColor Yellow
-                }} else {{ throw }}
-            }} 
-            
-            Write-Host "Concedendo SendAs a $usuario no shared $sharedSmtp..." -ForegroundColor Cyan
+            Write-Host "Concedendo SendAs a $usuario no shared $sharedSmtp " -ForegroundColor Cyan
             try {{
                 Add-RecipientPermission -Identity $sharedSmtp -Trustee $usuario -AccessRights SendAs -ErrorAction Stop
                 Write-Host "✓ SendAs concedido" -ForegroundColor Green
